cd99d84a4f8e6e4f1994d61d666baefd3187ac72
Rogelio <royer3020@gmail.com>
2020-07-17 10:17:46 -0500

    ayuuda

--- Crepas/Crepas/Form1.Designer.cs
+++ Crepas/Crepas/Form1.Designer.cs
@@ -32,7 +32,13 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pedidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.combo_productos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.combo_tipo = new System.Windows.Forms.ComboBox();
+            this.Pedidos = new System.Windows.Forms.ListBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,28 +56,91 @@
             // inicioToolStripMenuItem
             // 
             this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.inicioToolStripMenuItem.Text = "Inicio";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.inicioToolStripMenuItem.Text = "Incio";
             // 
             // pedidosToolStripMenuItem
             // 
             this.pedidosToolStripMenuItem.Name = "pedidosToolStripMenuItem";
-            this.pedidosToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.pedidosToolStripMenuItem.Text = "Pedidos";
+            this.pedidosToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.pedidosToolStripMenuItem.Text = "Cuentas";
+            // 
+            // combo_productos
+            // 
+            this.combo_productos.FormattingEnabled = true;
+            this.combo_productos.Location = new System.Drawing.Point(12, 79);
+            this.combo_productos.Name = "combo_productos";
+            this.combo_productos.Size = new System.Drawing.Size(121, 21);
+            this.combo_productos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Producto";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 64);
+            this.textBox1.Location = new System.Drawing.Point(13, 179);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tipo de pedido";
+            // 
+            // combo_tipo
+            // 
+            this.combo_tipo.FormattingEnabled = true;
+            this.combo_tipo.Items.AddRange(new object[] {
+            "Local",
+            "Domicilio"});
+            this.combo_tipo.Location = new System.Drawing.Point(13, 277);
+            this.combo_tipo.Name = "combo_tipo";
+            this.combo_tipo.Size = new System.Drawing.Size(121, 21);
+            this.combo_tipo.TabIndex = 6;
+            // 
+            // Pedidos
+            // 
+            this.Pedidos.FormattingEnabled = true;
+            this.Pedidos.Location = new System.Drawing.Point(424, 45);
+            this.Pedidos.Name = "Pedidos";
+            this.Pedidos.Size = new System.Drawing.Size(329, 316);
+            this.Pedidos.TabIndex = 7;
             // 
             // Inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pedidos);
+            this.Controls.Add(this.combo_tipo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combo_productos);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -90,7 +159,13 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem pedidosToolStripMenuItem;
+        private System.Windows.Forms.ComboBox combo_productos;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox combo_tipo;
+        private System.Windows.Forms.ListBox Pedidos;
     }
 }
 
