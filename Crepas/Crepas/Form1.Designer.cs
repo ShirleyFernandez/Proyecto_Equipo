a9af9f70af4f4dc657ad3c072ea1988517f2ee46
Rogelio <royer3020@gmail.com>
2020-07-20 11:00:43 -0500

    Este no

--- Crepas/Crepas/Form1.Designer.cs
+++ Crepas/Crepas/Form1.Designer.cs
@@ -235,6 +235,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 356);
             this.Controls.Add(this.btn_borrar);
             this.Controls.Add(this.Lista_Productos);
